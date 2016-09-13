41d6d824a4da77c955ddf571210edd2f9dc21dbc
johshisha <johshisha@gmail.com>
2016-09-13 13:32:20 +0900

    get user name

--- slack_echo.rb
+++ slack_echo.rb
@@ -1,4 +1,5 @@
 require 'slack'
+require 'pry'
 
 TOKEN = 'xoxb-78985393799-aMGpjloVH7QPdsMxS5X3EKDh'
 
@@ -8,6 +9,10 @@ end
 
 client = Slack.realtime
 
+users = Slack.users_list()['members']
+
+
+
 client.on :hello do
   puts 'Successfully connected.'
 end
@@ -15,7 +20,13 @@ end
 client.on :message do |data|
   if not data['username'] then
     # respond to messages
-    Slack.chat_postMessage(text: "#{data['text']}", channel: 'elevator_test')
+    users.each do | user |
+      if user['id'] == data['user'] then
+        Slack.chat_postMessage(text: "hello @#{user['name']}",
+          channel: 'elevator_test', link_names: 1, as_user: true)
+      end
+    end
+    #binding.pry
   end
 end
 
